52a2423a472cf8ddbb2fdce0915c7f4da7012cc2
Mykhel <mikkybrown300@gmail.com>
2022-09-12 08:52:31 -0700

    Print AA to Z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-char ch = "a";
+char ch = 'a';
 
-for (ch = "a"; ch <= "z"; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-putchar("\n");
+putchar('\n');
 return (0);
 }
