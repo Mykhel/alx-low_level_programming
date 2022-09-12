dae2e70f282686f96bf95c69e95dffe6a57341d1
Mykhel <mikkybrown300@gmail.com>
2022-09-12 06:05:35 -0700

    Print A-Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (successful)
+ */
+
+int main(void)
+{
+char ch = "a";
+
+for (ch = "a"; ch <= "z"; ch++)
+putchar(ch);
+putchar("\n");
+return (0);
+}
