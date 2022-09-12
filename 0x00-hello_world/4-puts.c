f935a6b29ff973cefae4e3f7d34cd9056715b745
Mykhel <mikkybrown300@gmail.com>
2022-09-12 01:15:17 -0700

    Outputting C program using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main -entry point
  *
@@ -7,7 +8,6 @@
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
-    
+puts("\"Programming is like building a multilingual puzzle");
+return (0);    
 }
