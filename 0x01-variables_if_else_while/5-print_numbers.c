f7cf427f85fba6d8a733866a8b4b182c63e51442
Mykhel <mikkybrown300@gmail.com>
2022-09-12 10:24:28 -0700

    print one digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (successfull)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+putchar('\n');
+return (0);
+}
