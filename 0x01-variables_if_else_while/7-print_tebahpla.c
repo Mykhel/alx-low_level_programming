9c7293cd8e9d5837ba23c0b50c9b4f7c774c7330
Mykhel <mikkybrown300@gmail.com>
2022-09-12 11:09:16 -0700

    A to Z reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch <= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
