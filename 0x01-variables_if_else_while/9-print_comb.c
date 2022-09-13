b0be04161a67cc4c5d4e7c18c808faba3620fb48
Mykhel <mikkybrown300@gmail.com>
2022-09-12 21:47:15 -0700

    Single digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (successfull)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+{
+putchar(i % 10 + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
