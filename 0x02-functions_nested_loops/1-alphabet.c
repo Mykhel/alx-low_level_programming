49cf8434d9cdf0e965c492db93a76d33e34e8c8b
Mykhel <mikkybrown300@gmail.com>
2022-09-14 19:27:56 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+
+ *print_alphabet -  entry point.
+
+ *Return : 0 (if successful)
+
+ */
+
+
+
+
+
+
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
