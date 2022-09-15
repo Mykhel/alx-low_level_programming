6fdeaa559b65fde7422282c86b1ad2cec08de836
Mykhel <mikkybrown300@gmail.com>
2022-09-14 19:15:37 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main-entry point
+ *
+ *Return:0 (successful)
+ */
+
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a project directory for C programming functions and loops
