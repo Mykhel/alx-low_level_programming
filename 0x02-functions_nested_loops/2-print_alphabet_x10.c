eeb42a9c3683b7a68b9a9f00ce3b51e43e7188ba
Mykhel <mikkybrown300@gmail.com>
2022-09-20 19:51:12 +0000

    Changes made to loop script

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,12 @@ void print_alphabet_x10(void)
 char c = 'a';
 int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 26; j++)
-        {
-            _putchar(c + j);
-        }
-        _putchar(10);
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 26; j++)
+{
+_putchar(c + j);
+}
+_putchar(10);
+}
 }
