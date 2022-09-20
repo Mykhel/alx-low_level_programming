79f5395cb5ee77b5a55190d2e932e33743a24a46
Mykhel <mikkybrown300@gmail.com>
2022-09-20 18:38:37 +0000

    Some changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
 
@@ -20,9 +19,9 @@ void print_alphabet(void)
 char c = 'a';
 int i;
 
-for (i = 0; i < 26; i++)
-{
-_putchar(c + i);
-}
-_putchar(10);
+    for (i = 0; i < 26; i++)
+    {
+        putchar(c + i);
+    }
+    putchar(10);
 }
