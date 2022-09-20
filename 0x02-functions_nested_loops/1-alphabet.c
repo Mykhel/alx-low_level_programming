1c666e79961b951e994763edb1e97f516ecb4014
Mykhel <mikkybrown300@gmail.com>
2022-09-20 19:42:05 +0000

    Changes made to loop script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,9 +13,9 @@ void print_alphabet(void)
 char c = 'a';
 int i;
 
-    for (i = 0; i < 26; i++)
-    {
-        putchar(c + i);
-    }
-    putchar(10);
+for (i = 0; i < 26; i++)
+{
+putchar(c + i);
+}
+putchar(10);
 }
