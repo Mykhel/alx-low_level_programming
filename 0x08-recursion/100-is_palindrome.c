1736b810957b7700e82c6fb9c311409dde519a7e
Mykhel <mikkybrown300@gmail.com>
2022-09-29 01:27:42 +0000

    palidrone

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,37 +15,37 @@ int is_palindrome(char *s)
 	return (palindrome_check(s, len - 1, i));
 }
 
-/**
- * _strlen_recursion - returns the length of a string
- *
- * @s: string
- * Return: int
- */
-int _strlen_recursion(char *s)
-{
-	int len;
+// /**
+//  * _strlen_recursion - returns the length of a string
+//  *
+//  * @s: string
+//  * Return: int
+//  */
+// int _strlen_recursion(char *s)
+// {
+// 	int len;
 
-	if (*s == '\0')
-		return (0);
-	len = _strlen_recursion(s + 1);
-	return (len + 1);
-}
+// 	if (*s == '\0')
+// 		return (0);
+// 	len = _strlen_recursion(s + 1);
+// 	return (len + 1);
+// }
 
-/**
- * palindrome_check - returns 1 if palindrome and 0 if not
- *
- * @s: string
- * @len: length of string
- * @i: counter variable
- * Return: int
- */
-int palindrome_check(char *s, int len, int i)
-{
-	if (len == i)
-		return (1);
-	else if (s[len] != s[i])
-		return (0);
-	if (i < len)
-		return (palindrome_check(s, --len, ++i));
-	return (1);
-}
+// /**
+//  * palindrome_check - returns 1 if palindrome and 0 if not
+//  *
+//  * @s: string
+//  * @len: length of string
+//  * @i: counter variable
+//  * Return: int
+//  */
+// int palindrome_check(char *s, int len, int i)
+// {
+// 	if (len == i)
+// 		return (1);
+// 	else if (s[len] != s[i])
+// 		return (0);
+// 	if (i < len)
+// 		return (palindrome_check(s, --len, ++i));
+// 	return (1);
+// }
