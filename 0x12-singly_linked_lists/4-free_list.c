2facce42c000729fe67cac08aacafd4ea598e3e5
Mykhel <mikkybrown300@gmail.com>
2022-10-22 01:07:39 +0000

    Linked lists script

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,12 +5,13 @@
 */
 void free_list(list_t *head)
 {
-list_t *temp;
-while (head != NULL)
-{
-temp = head;
-head = head->next;
-free(temp->str);
-free(temp);
+    list_t *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
 }
-}
+
