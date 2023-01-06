0f3afbcbc2e2b4b64c3c8185362cc5d71d2fbc8c
Mykhel <mikkybrown300@gmail.com>
2023-01-06 19:56:22 +0100

    Modified

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,34 @@
 #include "main.h"
-#include <string.h>
+
 /**
-*cap_string - function
-*@s : char params
-*Return: char  value
-*/
-
-char *cap_string(char *s)
+ * cap_string - capitalizes chars in a string following a separator
+ *
+ * @c: character string pointer
+ * Return: char pointer
+ */
+char *cap_string(char *c)
 {
-int n;
-char *ptr = s;
-int i, j = strlen(s);
-for (i = 0; i < j; i++)
-{
-    if (s[i] >= 97 && s[i] <= 122)
-    {
-        if (s[i - 1] == ',' || s[i - 1] == ';' ||  s[i - 1] == '.' ||
-        s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' ||
-        s[i -1] == '\t' ||
-        s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == ' ')
-        {
-            s[i] = (s[i] - 32);
-        }
-    }
-}
+	int i = 0, j,
+	sep[] = {32, '\t', 11,  '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-return (ptr);
-
+	if (c[0] > 96 && c[0] < 123)
+		c[0] -= 32;
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123)
+		{
+			j = 0;
+			while (j < 14)
+			{
+				if (c[i - 1] == sep[j])
+				{
+					c[i] -= 32;
+					break;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	return (c);
 }
